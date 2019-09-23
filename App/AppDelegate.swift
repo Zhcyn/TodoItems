a6e27ecd2ed0bebf6f78ce6acd555430c1727edb
Zhcyn <zhuoh_summer@163.com>
2019-09-23 11:56:41 +0800

    Add files via upload

---
+++ App/AppDelegate.swift
@@ -0,0 +1,72 @@
+//
+//  AppDelegate.swift
+//  Reminder
+//
+//  Created by nguyen.duc.huyb on 6/27/19.
+//  Copyright © 2019 nguyen.duc.huyb. All rights reserved.
+//
+
+import UserNotifications
+let appKey = "a85535fba2e53a23f8a1eb62"
+let channel = "Publish channel"
+let isProduction = true
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate ,JPUSHRegisterDelegate{
+
+    var window: UIWindow?
+    var blockRotation = Bool()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        RealmConfigure.configureMigration()
+        UNUserNotificationCenter.current().delegate = self
+        
+        
+        let entity = JPUSHRegisterEntity()
+        entity.types = NSInteger(UNAuthorizationOptions.alert.rawValue) |
+            NSInteger(UNAuthorizationOptions.sound.rawValue) |
+            NSInteger(UNAuthorizationOptions.badge.rawValue)
+        JPUSHService.register(forRemoteNotificationConfig: entity, delegate: self)
+        JPUSHService.setup(withOption: launchOptions, appKey: appKey, channel: channel, apsForProduction: isProduction)
+        _ = NotificationCenter.default
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+    }
+    
+    func jpushNotificationCenter(_ center: UNUserNotificationCenter!, willPresent notification: UNNotification!, withCompletionHandler completionHandler: ((Int) -> Void)!) {
+    }
+    @available(iOS 10.0, *)
+    func jpushNotificationCenter(_ center: UNUserNotificationCenter!, didReceive response: UNNotificationResponse!, withCompletionHandler completionHandler: (() -> Void)!) {
+    }
+    @available(iOS 10.0, *)
+    func jpushNotificationCenter(_ center: UNUserNotificationCenter!, openSettingsFor notification: UNNotification?) {
+    }
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("get the deviceToken  \(deviceToken)")
+        JPUSHService.registerDeviceToken(deviceToken)
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    //for displaying notification when app is in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    // For handling tap and user actions
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.identifier == Constant.localId.rawValue {
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
+    }
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if blockRotation {
+            return .allButUpsideDown
+        }
+        return .portrait
+    }
+}
